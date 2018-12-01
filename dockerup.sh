737e03a41283c4158cfd044b7df1e7f0bf20984d
Josh Smith <josh.smith@platocreative.co.nz>
2018-12-01 19:30:32 +1300

    Updated bash script to update .env file if it exists.
    + Uses sed to replace UID/GID values.
    + Still a _very_ rudimentary script.

--- dockerup.sh
+++ dockerup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-printf "UID=$(id -u)\nGID=$(id -g)" > .env && docker-compose up --build -d
+if [ ! -f ./.env ]; then
+    printf "UID=$(id -u)\nGID=$(id -g)" > .env
+else
+	sed -i '' -e 's@\(UID=\)\([0-9]*\)@\1'$(id -u)'@g' .env\
+	&& sed -i '' -e 's@\(GID=\)\([0-9]*\)@\1'$(id -g)'@g' .env
+fi
+
+docker-compose up --build -d
