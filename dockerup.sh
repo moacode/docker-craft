175df65182086d36223716a609d68dd410ab6a44
Josh Smith <josh.smith@platocreative.co.nz>
2018-12-02 19:47:10 +1300

    Updated dockerup.sh to append to existing .env file.

--- dockerup.sh
+++ dockerup.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f ./.env ]; then
     printf "UID=$(id -u)\nGID=$(id -g)" > .env
+elif ! grep -q "UID" .env; then
+    printf "\n\n# Special UID/GID variables for docker containers\nUID=$(id -u)\nGID=$(id -g)" >> .env
 else
 	sed -i '' -e 's@\(UID=\)\([0-9]*\)@\1'$(id -u)'@g' .env\
 	&& sed -i '' -e 's@\(GID=\)\([0-9]*\)@\1'$(id -g)'@g' .env
